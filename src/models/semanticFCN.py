e5b7cf793b0725411eeb16fe890e604029cf6e86
Hannes Reichert <103506308+kav-hareichert@users.noreply.github.com>
2024-05-15 12:23:28 +0200

    Added RegNet and ShuffleNet implementations. Made network definition more flexible

--- src/models/semanticFCN.py
+++ src/models/semanticFCN.py
@@ -4,23 +4,6 @@ import torch.nn.functional as F
 import torchvision.models as models
 import torch
 
-def remove_batchnorm(model):
-    # Create a new Sequential module to reconstruct the model architecture without batchnorm
-    new_model = nn.Sequential()
-    for name, module in model.named_children():
-        if isinstance(module, nn.BatchNorm2d):
-            # Skip batchnorm layers
-            continue
-        elif isinstance(module, nn.Sequential):
-            # If the module is Sequential, recursively remove batchnorm from its children
-            new_model.add_module(name, remove_batchnorm(module))
-        elif name in ["0","1","2"]:
-            new_model.add_module(name, remove_batchnorm(module))
-        else:
-            # Add other layers to the new model
-            new_model.add_module(name, module)
-            
-    return new_model
 
 class AttentionModule(nn.Module):
     def __init__(self, in_channels, out_channels):
@@ -61,7 +44,7 @@ class SemanticNetworkWithFPN(nn.Module):#
     Semantic Segmentation Network with Feature Pyramid Network (FPN) using a ResNet backbone.
 
     Args:
-        resnet_type (str): Type of ResNet model to use. Supported types: 'resnet18', 'resnet34', 'resnet50', 'resnet101'.
+        backbone (str): Type of ResNet model to use. Supported types: 'resnet18', 'resnet34', 'resnet50', 'resnet101'.
         meta_channel_dim (int): Number meta channels used in the FPN
         interpolation_mode (str): Interpolation mode used to resize the meta channels (default='nearest'). Supported types: 'nearest', 'bilinear', 'bicubic'.
         num_classes (int): Number of semantic classes
@@ -69,86 +52,121 @@ class SemanticNetworkWithFPN(nn.Module):#
         attention (bool): Option to use a attention mechanism in the FPN (see: https://arxiv.org/pdf/1706.03762.pdf)
         
     """
-    def __init__(self, resnet_type='resnet18', meta_channel_dim=3, interpolation_mode = 'nearest', num_classes = 3, no_bn=False, attention=True):
+    def __init__(self, backbone='resnet18', meta_channel_dim=3, interpolation_mode = 'nearest', num_classes = 3, attention=True):
         super(SemanticNetworkWithFPN, self).__init__()
 
+        self.backbone_name = backbone
         self.interpolation_mode = interpolation_mode
         self.num_classes = num_classes
-        self.no_bn = no_bn
         self.attention = attention
         # Load pre-trained ResNet model
-        if resnet_type == 'resnet18':
-            self.resnet = models.resnet18(pretrained=True)
-            base_channels = 512  # Number of channels in the last layer of ResNet18
-        elif resnet_type == 'resnet34':
-            self.resnet = models.resnet34(pretrained=True)
-            base_channels = 512  # Number of channels in the last layer of ResNet34
-        elif resnet_type == 'resnet50':
-            self.resnet = models.resnet50(pretrained=True)
-            base_channels = 2048  # Number of channels in the last layer of ResNet50
-        elif resnet_type == 'resnet101':
-            self.resnet = models.resnet101(pretrained=True)
-            base_channels = 2048  # Number of channels in the last layer of ResNet101
+        if backbone == 'resnet18':
+            self.backbone = models.resnet18(pretrained=True)
+            base_channel = 512  # Number of channels in the last layer of ResNet18
+            base_channels = [base_channel, base_channel // 2, base_channel // 4, base_channel // 8, base_channel // 16]
+        elif backbone == 'resnet34':
+            self.backbone = models.resnet34(pretrained=True)
+            base_channel = 512  # Number of channels in the last layer of ResNet34
+            base_channels = [base_channel, base_channel // 2, base_channel // 4, base_channel // 8, base_channel // 16]
+        elif backbone == 'resnet50':
+            self.backbone = models.resnet50(pretrained=True)
+            base_channel = 2048  # Number of channels in the last layer of ResNet50
+            base_channels = [base_channel, base_channel // 2, base_channel // 4, base_channel // 8, base_channel // 16]
+        elif backbone == 'regnet_y_400mf':
+            self.backbone = models.regnet_y_400mf(pretrained=True)
+            base_channels = [440, 208, 104, 48, 32]
+        elif backbone == 'regnet_y_800mf':
+            self.backbone = models.regnet_y_800mf(pretrained=True)
+            base_channels = [784, 320, 144, 64, 32]
+        elif backbone == 'regnet_y_1_6gf':
+            self.backbone = models.regnet_y_1_6gf(pretrained=True)
+            base_channels = [888, 336, 120, 48, 32]
+        elif backbone == 'regnet_y_3_2gf':
+            self.backbone = models.regnet_y_3_2gf(pretrained=True)
+            base_channels = [1512, 576, 216, 72, 32]
+        elif backbone == 'shufflenet_v2_x0_5':
+            self.backbone = models.shufflenet_v2_x0_5(pretrained=True)
+            base_channels = [1024, 192, 96, 48, 24]
+        elif backbone == 'shufflenet_v2_x1_0':
+            self.backbone = models.shufflenet_v2_x1_0(pretrained=True)
+            base_channels = [1024, 464, 232, 116, 24]
+        elif backbone == 'shufflenet_v2_x1_5':
+            self.backbone = models.shufflenet_v2_x1_5(pretrained=True)
+            base_channels = [1024, 704, 352, 176, 24]
+
 
         else:
-            raise ValueError("Invalid ResNet type. Supported types: 'resnet18', 'resnet34', 'resnet50', 'resnet101'.")
-
-        # remove all batchnorm layers
-        if self.no_bn:
-            self.resnet = remove_batchnorm(self.resnet)
+            raise ValueError("Invalid ResNet type. Supported types: 'resnet18', 'resnet34', 'resnet50', 'regnet_y_400mf','regnet_y_800mf', 'regnet_y_1_6gf', 'regnet_y_3_2gf', 'shufflenet_v2_x0_5', 'shufflenet_v2_x1_0', 'shufflenet_v2_x1_5'.")
         
         # Modify the first convolution layer to take 1+meta_channel_dim channels
         self.meta_channel_dim = meta_channel_dim
-        self.resnet.conv1 = nn.Conv2d(2 + meta_channel_dim, 64, kernel_size=3, stride=1, padding=1, bias=False)
 
-        # Extract feature maps from different layers of ResNet
-        self.layer1 = nn.Sequential(self.resnet.conv1, self.resnet.relu, self.resnet.maxpool, self.resnet.layer1)
-        self.layer2 = self.resnet.layer2
-        self.layer3 = self.resnet.layer3
-        self.layer4 = self.resnet.layer4
+        is_shuffle = False
+        # extract features from resnet family
+        if backbone in ['resnet18', 'resnet34', 'resnet50']:
+            self.backbone.conv1 = nn.Conv2d(2 + meta_channel_dim, 64, kernel_size=3, stride=1, padding=1, bias=False)
+
+            # Extract feature maps from different layers of ResNet
+            self.stem = nn.Sequential(self.backbone.conv1, self.backbone.relu, self.backbone.maxpool)
+            self.layer1 = self.backbone.layer1
+            self.layer2 = self.backbone.layer2
+            self.layer3 = self.backbone.layer3
+            self.layer4 = self.backbone.layer4
         
+        # extract features from regnet family
+        elif backbone in ['regnet_y_400mf','regnet_y_800mf', 'regnet_y_1_6gf', 'regnet_y_3_2gf']:
+            self.backbone.stem[0] = nn.Conv2d(2 + meta_channel_dim, self.backbone.stem[0].out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+            # Extract feature maps from different layers of RegNet
+            self.stem = self.backbone.stem
+            self.layer1 = self.backbone.trunk_output[0]
+            self.layer2 = self.backbone.trunk_output[1]
+            self.layer3 = self.backbone.trunk_output[2]
+            self.layer4 = self.backbone.trunk_output[3]
+
+        elif backbone in ["shufflenet_v2_x0_5", "shufflenet_v2_x1_0", "shufflenet_v2_x1_5"]:
+            self.backbone.conv1[0] = nn.Conv2d(2 + meta_channel_dim, self.backbone.conv1[0].out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+            #help_conv = nn.Conv2d(2 + meta_channel_dim, self.backbone.conv1[0].out_channels, kernel_size=3, stride=2, padding=1, bias=False)
+            # Extract feature maps using indices or named stages from ShuffleNet
+            self.stem = self.backbone.conv1
+            self.layer1 = self.backbone.stage2
+            self.layer2 = self.backbone.stage3
+            self.layer3 = self.backbone.stage4
+            self.layer4 = self.backbone.conv5
+            is_shuffle = True
+
         # Attention blocks
-        self.attention4 = AttentionModule(base_channels // 2, base_channels // 2)
-        self.attention3 = AttentionModule(base_channels // 4, base_channels // 4)
-        self.attention2 = AttentionModule(base_channels // 8, base_channels // 8)
-        self.attention1 = AttentionModule(base_channels // 16, base_channels // 16)
+        self.attention4 = AttentionModule(base_channels[1], base_channels[1])
+        self.attention3 = AttentionModule(base_channels[2], base_channels[2])
+        self.attention2 = AttentionModule(base_channels[3], base_channels[3])
+        self.attention1 = AttentionModule(base_channels[4], base_channels[4])
         
 
         # FPN blocks
-        self.fpn_block4 = self._make_fpn_block(base_channels, base_channels // 2)
-        self.fpn_block3 = self._make_fpn_block(base_channels // 2, base_channels // 4)
-        self.fpn_block2 = self._make_fpn_block(base_channels // 4, base_channels // 8)
-        self.fpn_block1 = self._make_fpn_block(base_channels // 8, base_channels // 16)
+        self.fpn_block4 = self._make_fpn_block(base_channels[0], base_channels[1])
+        self.fpn_block3 = self._make_fpn_block(base_channels[1], base_channels[2])
+        self.fpn_block2 = self._make_fpn_block(base_channels[2], base_channels[3])
+        self.fpn_block1 = self._make_fpn_block(base_channels[3], base_channels[4])
 
         # upsamle layers
-        self.upsample_layer_x4 = nn.ConvTranspose2d(in_channels=base_channels // 2, out_channels=base_channels // 2, kernel_size=8, stride=8, padding=0)
-        self.upsample_layer_x3 = nn.ConvTranspose2d(in_channels=base_channels // 4, out_channels=base_channels // 4, kernel_size=4, stride=4, padding=0)
-        self.upsample_layer_x2 = nn.ConvTranspose2d(in_channels=base_channels // 8, out_channels=base_channels // 8, kernel_size=2, stride=2, padding=0)
-        
-        if self.no_bn:
-            self.decoder_semantic = nn.Sequential(
-                nn.Conv2d(base_channels // 2 + base_channels // 4 + base_channels // 8 + base_channels // 16, base_channels // 16, kernel_size=3, stride=1, padding=1),
-                #nn.Conv2d(base_channels // 8 + base_channels // 16, base_channels // 16, kernel_size=3, stride=1, padding=1),
-                #nn.BatchNorm2d(base_channels // 16),
-                nn.ReLU(inplace=True),
-                nn.Conv2d(base_channels // 16, base_channels // 16, kernel_size=3, stride=1, padding=1),
-                #nn.BatchNorm2d(base_channels // 16),
-                nn.ReLU(inplace=True),
-                nn.ConvTranspose2d(base_channels // 16, self.num_classes, kernel_size=4, stride=2, padding=1),
-                nn.ELU(inplace=True)
-            )
+        if is_shuffle:
+            self.upsample_layer_x4 = nn.ConvTranspose2d(in_channels=base_channels[1], out_channels=base_channels[1], kernel_size=4, stride=4, padding=0)
         else:
-            self.decoder_semantic = nn.Sequential(
-                nn.Conv2d(base_channels // 2 + base_channels // 4 + base_channels // 8 + base_channels // 16, base_channels // 16, kernel_size=3, stride=1, padding=1),
-                #nn.Conv2d(base_channels // 8 + base_channels // 16, base_channels // 16, kernel_size=3, stride=1, padding=1),
-                nn.BatchNorm2d(base_channels // 16),
-                nn.ReLU(inplace=True),
-                nn.Conv2d(base_channels // 16, base_channels // 16, kernel_size=3, stride=1, padding=1),
-                nn.BatchNorm2d(base_channels // 16),
-                nn.ReLU(inplace=True),
-                nn.ConvTranspose2d(base_channels // 16, self.num_classes, kernel_size=4, stride=2, padding=1),
-                nn.ELU(inplace=True)
-            )
+            self.upsample_layer_x4 = nn.ConvTranspose2d(in_channels=base_channels[1], out_channels=base_channels[1], kernel_size=8, stride=8, padding=0)
+        self.upsample_layer_x3 = nn.ConvTranspose2d(in_channels=base_channels[2], out_channels=base_channels[2], kernel_size=4, stride=4, padding=0)
+        self.upsample_layer_x2 = nn.ConvTranspose2d(in_channels=base_channels[3], out_channels=base_channels[3], kernel_size=2, stride=2, padding=0)
+        
+
+        self.decoder_semantic = nn.Sequential(
+            nn.Conv2d(base_channels[1] + base_channels[2] + base_channels[3] + base_channels[4], base_channels[4], kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(base_channels[4]),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(base_channels[4], base_channels[4], kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(base_channels[4]),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(base_channels[4], self.num_classes, kernel_size=4, stride=2, padding=1),
+            nn.ELU(inplace=True)
+        )
+
     def _make_fpn_block(self, in_channels, out_channels):
         """
         Create an FPN block.
@@ -160,18 +178,12 @@ class SemanticNetworkWithFPN(nn.Module):#
         Returns:
             nn.Module: FPN block.
         """
-        if self.no_bn:
-            return nn.Sequential(
-                nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1),
-                #nn.BatchNorm2d(out_channels),
-                nn.ReLU(inplace=True),
-            )
-        else:
-            return nn.Sequential(
-                nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1),
-                nn.BatchNorm2d(out_channels),
-                nn.ReLU(inplace=True),
-            )
+
+        return nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+        )
             
     def forward(self, x, meta_channel):
         """
@@ -193,7 +205,8 @@ class SemanticNetworkWithFPN(nn.Module):#
             meta_channel2 = F.interpolate(meta_channel, scale_factor=1/4, mode=self.interpolation_mode)
             meta_channel3 = F.interpolate(meta_channel, scale_factor=1/8, mode=self.interpolation_mode)
             x = torch.cat([x, meta_channel], dim=1)
-            x1 = self.layer1(x)
+            xs = self.stem(x)
+            x1 = self.layer1(xs)
             x = torch.cat([x1[:,0:-self.meta_channel_dim,...], meta_channel1], dim=1)
             x2 = self.layer2(x)
             x = torch.cat([x2[:,0:-self.meta_channel_dim,...], meta_channel2], dim=1)
@@ -204,7 +217,8 @@ class SemanticNetworkWithFPN(nn.Module):#
         else:
         
             # Encoder (ResNet)
-            x1 = self.layer1(x)
+            xs = self.stem(x)
+            x1 = self.layer1(xs)
             x2 = self.layer2(x1)
             x3 = self.layer3(x2)
             x4 = self.layer4(x3)
@@ -221,6 +235,8 @@ class SemanticNetworkWithFPN(nn.Module):#
             x3 = self.attention3(x3)
             x2 = self.attention2(x2)
             x1 = self.attention1(x1)
+
+        
 
         x4 = self.upsample_layer_x4(x4)
         x3 = self.upsample_layer_x3(x3)
