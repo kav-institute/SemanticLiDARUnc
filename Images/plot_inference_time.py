eeae20250dbd126cbd53a1c4bd3c6b18f186b608
Hannes Reichert <103506308+kav-hareichert@users.noreply.github.com>
2024-05-21 08:50:50 +0200

    Update plot_inference_time.py

--- Images/plot_inference_time.py
+++ Images/plot_inference_time.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 
 # Data
-models = ['shufflenet_v2_x0_5', 'regnet_y_400mf']
-representation = ["shufflenet", "regnet"]
-inference_times = [11.2, 15.7]  # in milliseconds
-mIoU_scores = [42.9, 51]
-num_parameters = [4.6, 8.6]  # in millions
+models = ['resnet50', 'regnet_y_1_6gf','regnet_y_3_2gf','shufflenet_v2_x1_5','resnet34','regnet_y_800mf','shufflenet_v2_x1_0']
+representation = ["resnet", "regnet", "regnet","shufflenet","resnet", "regnet","shufflenet"]
+inference_times = [43.7, 21.7, 25.1, 23.6, 13.6, 14.4, 15.1]  # in milliseconds
+mIoU_scores = [60.07, 55.78, 55.69, 59.38, 57.3, 55.64, 60.0]
+num_parameters = [128.8, 22.25, 52, 25.1, 28.3, 16.7, 13.2]  # in millions
 
 # Scaling the number of parameters for dot size
 sizes = [param * 10 for param in num_parameters]  # arbitrary scaling factor for better visualization
@@ -28,8 +28,8 @@ for i, model in enumerate(models):
     ax.annotate(model, (inference_times[i], mIoU_scores[i]))
 
 # Set the axes limits
-ax.set_xlim(0, 100)
-ax.set_ylim(20, 80)
+ax.set_xlim(0, 60)
+ax.set_ylim(52, 65)
 
 # Adding legend
 #legend1 = ax.legend(*scatter.legend_elements(),
