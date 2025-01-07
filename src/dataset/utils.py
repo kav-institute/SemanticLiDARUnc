b0f74c724a5a11305619fdffe93a22caa781d755
Hannes Reichert <103506308+kav-hareichert@users.noreply.github.com>
2025-01-07 13:16:53 +0100

    Update utils.py

--- src/dataset/utils.py
+++ src/dataset/utils.py
@@ -1,130 +1,210 @@
 import numpy as np
-import cv2
 
-def rotate_z(point_cloud, angle):
-    # Convert angle to radians
-    angle_rad = np.radians(angle)
-    
-    # Define the rotation matrix
-    rotation_matrix = np.array([
-        [np.cos(angle_rad), -np.sin(angle_rad), 0],
-        [np.sin(angle_rad), np.cos(angle_rad), 0],
-        [0, 0, 1]
-    ])
-    
-    # Apply the rotation matrix to the point cloud
-    rotated_cloud = np.dot(point_cloud, rotation_matrix)
-    
-    return rotated_cloud
+id_map = {
+  0 : 0,     # "unlabeled"
+  1 : 0,    # "outlier" mapped to "unlabeled" --------------------------mapped
+  10: 1,     # "car"
+  11: 2,     # "bicycle"
+  13: 5,     # "bus" mapped to "other-vehicle" --------------------------mapped
+  15: 3,     # "motorcycle"
+  16: 5,     # "on-rails" mapped to "other-vehicle" ---------------------mapped
+  18: 4,     # "truck"
+  20: 5,     # "other-vehicle"
+  30: 6,     # "person"
+  31: 7,     # "bicyclist"
+  32: 8,     # "motorcyclist"
+  40: 9,     # "road"
+  44: 10,    # "parking"
+  48: 11,    # "sidewalk"
+  49: 12,    # "other-ground"
+  50: 13,    # "building"
+  51: 14,    # "fence"
+  52: 0,     # "other-structure" mapped to "unlabeled" ------------------mapped
+  60: 9,     # "lane-marking" to "road" ---------------------------------mapped
+  70: 15,    # "vegetation"
+  71: 16,    # "trunk"
+  72: 17,    # "terrain"
+  80: 18,    # "pole"
+  81: 19,    # "traffic-sign"
+  99: 0,     # "other-object" to "unlabeled" ----------------------------mapped
+  252: 1,    # "moving-car" to "car" ------------------------------------mapped
+  253: 7,    # "moving-bicyclist" to "bicyclist" ------------------------mapped
+  254: 6,    # "moving-person" to "person" ------------------------------mapped
+  255: 8,    # "moving-motorcyclist" to "motorcyclist" ------------------mapped
+  256: 5,    # "moving-on-rails" mapped to "other-vehicle" --------------mapped
+  257: 5,    # "moving-bus" mapped to "other-vehicle" -------------------mapped
+  258: 4,    # "moving-truck" to "truck" --------------------------------mapped
+  259: 5,    # "moving-other"-vehicle to "other-vehicle" ----------------mapped
+}
 
+id_map_reduced = {
+  0 : 0,     # "unlabeled"
+  1 : 0,    # "outlier" mapped to "unlabeled" --------------------------mapped
+  10: 1,     # "car"
+  11: 2,     # "bicycle" mapped to "two-wheeled -------------------mapped
+  13: 3,     # "bus" mapped to "other-vehicle" --------------------------mapped
+  15: 2,     # "motorcycle" mapped to "two-wheeled -------------------mapped
+  16: 3,     # "on-rails" mapped to "other-vehicle" ---------------------mapped
+  18: 3,     # "truck" mapped to "other-vehicle" ---------------------mapped
+  20: 3,     # "other-vehicle"
+  30: 4,     # "person" 
+  31: 5,     # "bicyclist" mapped to "rider" ---------------------mapped
+  32: 5,     # "motorcyclist" mapped to "rider" ---------------------mapped
+  40: 6,     # "road"
+  44: 6,    # "parking"
+  48: 7,    # "sidewalk"
+  49: 8,    # "other-ground"
+  50: 9,    # "building"
+  51: 9,    # "fence"
+  52: 0,     # "other-structure" mapped to "unlabeled" ------------------mapped
+  60: 6,     # "lane-marking" to "road" ---------------------------------mapped
+  70: 7,    # "vegetation"
+  71: 7,    # "trunk"
+  72: 10,    # "terrain"
+  80: 11,    # "pole"
+  81: 12,    # "traffic-sign"
+  99: 0,     # "other-object" to "unlabeled" ----------------------------mapped
+  252: 1,    # "moving-car" to "car" ------------------------------------mapped
+  253: 5,    # "moving-bicyclist" to "rider" ------------------------mapped
+  254: 6,    # "moving-person" to "person" ------------------------------mapped
+  255: 5,    # "moving-motorcyclist" to "rider" ------------------mapped
+  256: 3,    # "moving-on-rails" mapped to "other-vehicle" --------------mapped
+  257: 3,    # "moving-bus" mapped to "other-vehicle" -------------------mapped
+  258: 3,    # "moving-truck" to "other-vehicle"" -----------------------mapped
+  259: 3,    # "moving-other"-vehicle to "other-vehicle" ----------------mapped
+}
 
-def rotate_equirectangular_image(image, angle):
-    # Calculate the shift amount based on the angle of rotation
-    shift_amount = int(round((angle / (2 * np.pi)) * image.shape[1]))
+id_map_dynamic = {
+  0 : 0,     # "unlabeled"
+  1 : 0,    # "outlier" mapped to "unlabeled" --------------------------mapped
+  10: 1,     # "car"
+  11: 2,     # "bicycle"
+  13: 5,     # "bus" mapped to "other-vehicle" --------------------------mapped
+  15: 3,     # "motorcycle"
+  16: 5,     # "on-rails" mapped to "other-vehicle" ---------------------mapped
+  18: 4,     # "truck"
+  20: 5,     # "other-vehicle"
+  30: 6,     # "person"
+  31: 7,     # "bicyclist"
+  32: 8,     # "motorcyclist"
+  40: 0,     # "road"
+  44: 0,    # "parking"
+  48: 0,    # "sidewalk"
+  49: 0,    # "other-ground"
+  50: 0,    # "building"
+  51: 0,    # "fence"
+  52: 0,     # "other-structure" mapped to "unlabeled" ------------------mapped
+  60: 0,     # "lane-marking" to "road" ---------------------------------mapped
+  70: 0,    # "vegetation"
+  71: 0,    # "trunk"
+  72: 0,    # "terrain"
+  80: 0,    # "pole"
+  81: 0,    # "traffic-sign"
+  99: 0,     # "other-object" to "unlabeled" ----------------------------mapped
+  252: 1,    # "moving-car" to "car" ------------------------------------mapped
+  253: 7,    # "moving-bicyclist" to "bicyclist" ------------------------mapped
+  254: 6,    # "moving-person" to "person" ------------------------------mapped
+  255: 8,    # "moving-motorcyclist" to "motorcyclist" ------------------mapped
+  256: 5,    # "moving-on-rails" mapped to "other-vehicle" --------------mapped
+  257: 5,    # "moving-bus" mapped to "other-vehicle" -------------------mapped
+  258: 4,    # "moving-truck" to "truck" --------------------------------mapped
+  259: 5,    # "moving-other"-vehicle to "other-vehicle" ----------------mapped
+}
 
-    # Roll the image around the width axis
-    rotated_image = np.roll(image, shift_amount, axis=1)
+color_map = {
+  0 : [0, 0, 0],
+  1 : [245, 150, 100],
+  2 : [245, 230, 100],
+  3 : [150, 60, 30],
+  4 : [180, 30, 80],
+  5 : [255, 0, 0],
+  6: [30, 30, 255],
+  7: [200, 40, 255],
+  8: [90, 30, 150],
+  9: [125,125,125],#[255, 0, 255],
+  10: [255, 150, 255],
+  11: [75, 0, 75],
+  12: [75, 0, 175],
+  13: [0, 200, 255],
+  14: [50, 120, 255],
+  15: [0, 175, 0],
+  16: [0, 60, 135],
+  17: [80, 240, 150],
+  18: [150, 240, 255],
+  19: [250, 10, 250],
+  20: [255, 255, 255]
+}
 
-    return rotated_image
+color_map_reduced = {
+  0 : [0, 0, 0], # none
+  1 : [245, 150, 100], # car
+  2 : [245, 230, 100], # two-wheeled
+  3 : [255, 0, 0], # other-vehicle
+  4 : [30, 30, 255], # person
+  5 : [200, 40, 255], # rider
+  6: [125,125,125], # road
+  7: [75, 0, 75], # sidewalk
+  8: [255, 150, 255], # other-ground
+  9: [0, 175, 0], # vegetation
+  10: [0, 60, 135], # terrain
+  11: [150, 240, 255], # pole
+  12: [250, 250, 250] # traffic-sign
+}
 
-def build_normal_xyz(xyz, norm_factor=0.25, ksize = 3):
-    '''
-    @param xyz: ndarray with shape (h,w,3) containing a stagged point cloud
-    @param norm_factor: int for the smoothing in Schaar filter
-    '''
-    x = xyz[...,0]
-    y = xyz[...,1]
-    z = xyz[...,2]
+class_names = {
+  0 : "unlabeled",
+  1 : "car",
+  2 : "bicycle",
+  3 : "motorcycle",
+  4 : "truck",
+  5 : "other-vehicle",
+  6: "person",
+  7: "bicyclist",
+  8: "motorcyclist",
+  9: "road",
+  10: "parking",
+  11: "sidewalk",
+  12: "other-ground",
+  13: "building",
+  14: "fence",
+  15: "vegetation",
+  16: "trunk",
+  17: "terrain",
+  18: "pole",
+  19: "traffic-sign",
+}
 
-    Sxx = cv2.Scharr(x.astype(np.float32), cv2.CV_32FC1, 1, 0, scale=1.0/norm_factor)    
-    Sxy = cv2.Scharr(x.astype(np.float32), cv2.CV_32FC1, 0, 1, scale=1.0/norm_factor)
+# color_map = {
+#   0 : [0, 0, 0],
+#   1 : [245, 150, 100],
+#   2 : [245, 230, 100],
+#   3 : [150, 60, 30],
+#   4 : [180, 30, 80],
+#   5 : [255, 0, 0],
+#   6: [30, 30, 255],
+#   7: [200, 40, 255],
+#   8: [90, 30, 150],
+#   9: [125,125,125],
+#   10: [255, 150, 255],
+#   11: [75, 0, 75],
+#   12: [75, 0, 175],
+#   13: [0, 200, 255],
+#   14: [50, 120, 255],
+#   15: [0, 175, 0],
+#   16: [0, 60, 135],
+#   17: [80, 240, 150],
+#   18: [150, 240, 255],
+#   19: [250, 250, 250],
+#   20: [0, 250, 0]
+# }
 
-    Syx = cv2.Scharr(y.astype(np.float32), cv2.CV_32FC1, 1, 0, scale=1.0/norm_factor)    
-    Syy = cv2.Scharr(y.astype(np.float32), cv2.CV_32FC1, 0, 1, scale=1.0/norm_factor)
+# Create the custom color map
+custom_colormap = np.zeros((256, 1, 3), dtype=np.uint8)
 
-    Szx = cv2.Scharr(z.astype(np.float32), cv2.CV_32FC1, 1, 0, scale=1.0/norm_factor)    
-    Szy = cv2.Scharr(z.astype(np.float32), cv2.CV_32FC1, 0, 1, scale=1.0/norm_factor)
-
-    #build cross product
-    normal = -np.dstack((Syx*Szy - Szx*Syy,
-                        Szx*Sxy - Szy*Sxx,
-                        Sxx*Syy - Syx*Sxy))
-
-    # normalize corss product
-    n = np.linalg.norm(normal, axis=2)+1e-10
-    normal[:, :, 0] /= n
-    normal[:, :, 1] /= n
-    normal[:, :, 2] /= n
-    
-    return normal
-
-def to_deflection_coordinates(x,y,z):
-    # To cylindrical
-    p = np.sqrt(x ** 2 + y ** 2)
-    phi = np.arctan2(y, x)
-    # To spherical   
-    theta = -np.arctan2(p, z) + np.pi/2
-    return phi, theta
-
-def spherical_projection(pc, height=64, width=2048, theta_range=None, th=1.0, sort_largest_first=False, bins_h=None, max_range=None):
-    '''spherical projection 
-    Args:
-        pc: point cloud, dim: N*C
-    Returns:
-        pj_img: projected spherical iamges, shape: h*w*C
-    '''
-
-    # filter all small range values to avoid overflows in theta min max calculation
-    #if isinstance(theta_range, type(None)):
-        
-    r = np.sqrt(pc[:, 0] ** 2 + pc[:, 1] ** 2 + pc[:, 2] ** 2)
-    arr1inds = r.argsort()
-    if sort_largest_first:
-        pc = pc[arr1inds]
+for i in range(256):
+    if i in color_map:
+        custom_colormap[i, 0, :] = color_map[i]
     else:
-        pc = pc[arr1inds[::-1]]
-    #pc = pc[arr1inds]
-    # r = np.sqrt(pc[:, 0] ** 2 + pc[:, 1] ** 2 + pc[:, 2] ** 2)
-    # if not isinstance(max_range,type(None)):
-    #     indices = np.where((r > th)*(r<=max_range))
-    # else:
-    #     indices = np.where(r > th)
-    # pc = pc[indices]
-        
-    x = pc[:, 0]
-    y = pc[:, 1]
-    z = pc[:, 2]
-
-    r = np.sqrt(x ** 2 + y ** 2 + z ** 2)
-        
-    phi, theta = to_deflection_coordinates(x,y,z)
-
-    #indices = np.where(r > th)
-    if isinstance(theta_range, type(None)):
-        theta_min, theta_max = [theta.min(), theta.max()]
-    else: 
-        theta_min, theta_max = theta_range
-        
-    phi_min, phi_max = [-np.pi, np.pi]
-    
-    # assuming uniform distribution of rays
-    if isinstance(bins_h, type(None)):
-        bins_h = np.linspace(theta_min, theta_max, height)[::-1]
-        
-    bins_w = np.linspace(phi_min, phi_max, width)[::-1]
-    
-    theta_img = np.stack(width*[bins_h], axis=-1)
-    phi_img = np.stack(height*[bins_w], axis=0)
-
-    idx_h = np.digitize(theta, bins_h)-1
-    idx_w = np.digitize(phi, bins_w)-1
-    
-    pj_img = np.zeros((height, width, pc.shape[1])).astype(np.float32)
-
-    
-    pj_img[idx_h, idx_w, :] = pc
-
-   
-    alpha = np.sqrt(np.square(theta_img)+np.square(phi_img))
-   
-    return pj_img, alpha, (theta_min, theta_max), (phi_min, phi_max) 
+        # If the index is not defined in the color map, set it to black
+        custom_colormap[i, 0, :] = [0, 0, 0]
+custom_colormap = custom_colormap[...,::-1]
