62c24d0626e71950b0fa85d945b73ad1c915c94a
Hannes Reichert <103506308+kav-hareichert@users.noreply.github.com>
2025-01-16 09:35:34 +0100

    Update run_training_THAB.sh

--- src/run_training_THAB.sh
+++ src/run_training_THAB.sh
@@ -13,12 +13,12 @@ SMALL_NUM_WORKERS=8
 
 # Array of model types
 MODEL_TYPES=(
-    #'resnet50'
-    #'shufflenet_v2_x1_5'
+    'resnet50'
+    'shufflenet_v2_x1_5'
     'resnet34'
-    #'shufflenet_v2_x1_0'
-    #'resnet18'
-    #'shufflenet_v2_x0_5'
+    'shufflenet_v2_x1_0'
+    'resnet18'
+    'shufflenet_v2_x0_5'
 )
 
 DATASETS=(
@@ -28,7 +28,8 @@ DATASETS=(
     "3"
     "4"
     "5"
-    "-1"
+    "6"
+    "7"
 )
 
 
@@ -46,13 +47,13 @@ do
     # Loop through combinations of --attention and --normals flags
     for DATASET in "${DATASETS[@]}"
         do
-        for PRETRAIN_FLAG in "--pretrained" ""
+        for PRETRAIN_FLAG in "--pretrained"
         do
-            for ATTENTION_FLAG in "--attention"
+            for ATTENTION_FLAG in "--attention" #""
             do
-                for NORMALS_FLAG in "--normals" ""
+                for NORMALS_FLAG in "--normals" #""
                 do
-                    for MULTI_SCALE_FLAG in "--multi_scale_meta" ""
+                    for MULTI_SCALE_FLAG in "--multi_scale_meta" #""
                     do
                         echo "Training with model: $MODEL_TYPE, Batch size: $BATCH_SIZE, Num workers: $NUM_WORKERS, Attention: $ATTENTION_FLAG, Normals: $NORMALS_FLAG, Pretrain: $PRETRAIN_FLAG, Split:  $DATASET"
                         python $SCRIPT_PATH --model_type $MODEL_TYPE --learning_rate $LEARNING_RATE --num_epochs $NUM_EPOCHS --batch_size $BATCH_SIZE --num_workers $NUM_WORKERS --test_id $DATASET --flip $ATTENTION_FLAG $NORMALS_FLAG $MULTI_SCALE_FLAG $PRETRAIN_FLAG
