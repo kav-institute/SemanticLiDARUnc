c5b4443c9aef175f3153a6be0f075127ba6ff4b4
Hannes Reichert <103506308+kav-hareichert@users.noreply.github.com>
2025-04-14 09:32:58 +0200

    Update tester.py

--- src/models/tester.py
+++ src/models/tester.py
@@ -1,212 +1,116 @@
+
+import torch
+import time
+import os
+import json
 import numpy as np
+from tqdm import tqdm
+import cv2
 
-id_map = {
-  0 : 0,     # "unlabeled"
-  1 : 0,    # "outlier" mapped to "unlabeled" --------------------------mapped
-  9: 0,
-  10: 1,     # "car"
-  11: 2,     # "bicycle"
-  13: 5,     # "bus" mapped to "other-vehicle" --------------------------mapped
-  15: 3,     # "motorcycle"
-  16: 5,     # "on-rails" mapped to "other-vehicle" ---------------------mapped
-  18: 4,     # "truck"
-  20: 5,     # "other-vehicle"
-  30: 6,     # "person"
-  31: 7,     # "bicyclist"
-  32: 8,     # "motorcyclist"
-  40: 9,     # "road"
-  44: 10,    # "parking"
-  48: 11,    # "sidewalk"
-  49: 12,    # "other-ground"
-  50: 13,    # "building"
-  51: 14,    # "fence"
-  52: 0,     # "other-structure" mapped to "unlabeled" ------------------mapped
-  60: 19,     # "lane-marking" to "traffic-sign" ------------------------mapped
-  70: 15,    # "vegetation"
-  71: 16,    # "trunk"
-  72: 17,    # "terrain"
-  80: 18,    # "pole"
-  81: 19,    # "traffic-sign"
-  99: 0,     # "other-object" to "unlabeled" ----------------------------mapped
-  252: 1,    # "moving-car" to "car" ------------------------------------mapped
-  253: 7,    # "moving-bicyclist" to "bicyclist" ------------------------mapped
-  254: 6,    # "moving-person" to "person" ------------------------------mapped
-  255: 8,    # "moving-motorcyclist" to "motorcyclist" ------------------mapped
-  256: 5,    # "moving-on-rails" mapped to "other-vehicle" --------------mapped
-  257: 5,    # "moving-bus" mapped to "other-vehicle" -------------------mapped
-  258: 4,    # "moving-truck" to "truck" --------------------------------mapped
-  259: 5,    # "moving-other"-vehicle to "other-vehicle" ----------------mapped
-}
+from models.evaluator import SemanticSegmentationEvaluator
 
-id_map_reduced = {
-  0 : 0,     # "unlabeled"
-  1 : 0,    # "outlier" mapped to "unlabeled" --------------------------mapped
-  9: 0,
-  10: 1,     # "car"
-  11: 2,     # "bicycle" mapped to "two-wheeled -------------------mapped
-  13: 3,     # "bus" mapped to "other-vehicle" --------------------------mapped
-  15: 2,     # "motorcycle" mapped to "two-wheeled -------------------mapped
-  16: 3,     # "on-rails" mapped to "other-vehicle" ---------------------mapped
-  18: 3,     # "truck" mapped to "other-vehicle" ---------------------mapped
-  20: 3,     # "other-vehicle"
-  30: 4,     # "person" 
-  31: 5,     # "bicyclist" mapped to "rider" ---------------------mapped
-  32: 5,     # "motorcyclist" mapped to "rider" ---------------------mapped
-  40: 6,     # "road"
-  44: 6,    # "parking"
-  48: 7,    # "sidewalk"
-  49: 8,    # "other-ground"
-  50: 9,    # "building"
-  51: 9,    # "fence"
-  52: 0,     # "other-structure" mapped to "unlabeled" ------------------mapped
-  60: 6,     # "lane-marking" to "road" ---------------------------------mapped
-  70: 7,    # "vegetation"
-  71: 7,    # "trunk"
-  72: 10,    # "terrain"
-  80: 11,    # "pole"
-  81: 12,    # "traffic-sign"
-  99: 0,     # "other-object" to "unlabeled" ----------------------------mapped
-  252: 1,    # "moving-car" to "car" ------------------------------------mapped
-  253: 5,    # "moving-bicyclist" to "rider" ------------------------mapped
-  254: 6,    # "moving-person" to "person" ------------------------------mapped
-  255: 5,    # "moving-motorcyclist" to "rider" ------------------mapped
-  256: 3,    # "moving-on-rails" mapped to "other-vehicle" --------------mapped
-  257: 3,    # "moving-bus" mapped to "other-vehicle" -------------------mapped
-  258: 3,    # "moving-truck" to "other-vehicle"" -----------------------mapped
-  259: 3,    # "moving-other"-vehicle to "other-vehicle" ----------------mapped
-}
+def visualize_semantic_segmentation_cv2(mask, class_colors):
+    """
+    Visualize semantic segmentation mask using class colors with cv2.
 
-id_map_dynamic = {
-  0 : 0,     # "unlabeled"
-  1 : 0,    # "outlier" mapped to "unlabeled" --------------------------mapped
-  10: 1,     # "car"
-  11: 2,     # "bicycle"
-  13: 5,     # "bus" mapped to "other-vehicle" --------------------------mapped
-  15: 3,     # "motorcycle"
-  16: 5,     # "on-rails" mapped to "other-vehicle" ---------------------mapped
-  18: 4,     # "truck"
-  20: 5,     # "other-vehicle"
-  30: 6,     # "person"
-  31: 7,     # "bicyclist"
-  32: 8,     # "motorcyclist"
-  40: 0,     # "road"
-  44: 0,    # "parking"
-  48: 0,    # "sidewalk"
-  49: 0,    # "other-ground"
-  50: 0,    # "building"
-  51: 0,    # "fence"
-  52: 0,     # "other-structure" mapped to "unlabeled" ------------------mapped
-  60: 0,     # "lane-marking" to "road" ---------------------------------mapped
-  70: 0,    # "vegetation"
-  71: 0,    # "trunk"
-  72: 0,    # "terrain"
-  80: 0,    # "pole"
-  81: 0,    # "traffic-sign"
-  99: 0,     # "other-object" to "unlabeled" ----------------------------mapped
-  252: 1,    # "moving-car" to "car" ------------------------------------mapped
-  253: 7,    # "moving-bicyclist" to "bicyclist" ------------------------mapped
-  254: 6,    # "moving-person" to "person" ------------------------------mapped
-  255: 8,    # "moving-motorcyclist" to "motorcyclist" ------------------mapped
-  256: 5,    # "moving-on-rails" mapped to "other-vehicle" --------------mapped
-  257: 5,    # "moving-bus" mapped to "other-vehicle" -------------------mapped
-  258: 4,    # "moving-truck" to "truck" --------------------------------mapped
-  259: 5,    # "moving-other"-vehicle to "other-vehicle" ----------------mapped
-}
+    Parameters:
+    - mask: 2D NumPy array containing class IDs for each pixel.
+    - class_colors: Dictionary mapping class IDs to BGR colors.
 
-color_map = {
-  0 : [0, 0, 0],
-  1 : [245, 150, 100],
-  2 : [245, 230, 100],
-  3 : [150, 60, 30],
-  4 : [180, 30, 80],
-  5 : [255, 0, 0],
-  6: [30, 30, 255],
-  7: [200, 40, 255],
-  8: [90, 30, 150],
-  9: [125,125,125],#[255, 0, 255],
-  10: [255, 150, 255],
-  11: [75, 0, 75],
-  12: [75, 0, 175],
-  13: [0, 200, 255],
-  14: [50, 120, 255],
-  15: [0, 175, 0],
-  16: [0, 60, 135],
-  17: [80, 240, 150],
-  18: [150, 240, 255],
-  19: [250, 10, 250],
-  20: [255, 255, 255]
-}
+    Returns:
+    - visualization: Colored semantic segmentation image in BGR format.
+    """
+    h, w = mask.shape
+    visualization = np.zeros((h, w, 3), dtype=np.uint8)
 
-color_map_reduced = {
-  0 : [0, 0, 0], # none
-  1 : [245, 150, 100], # car
-  2 : [245, 230, 100], # two-wheeled
-  3 : [255, 0, 0], # other-vehicle
-  4 : [30, 30, 255], # person
-  5 : [200, 40, 255], # rider
-  6: [125,125,125], # road
-  7: [75, 0, 75], # sidewalk
-  8: [255, 150, 255], # other-ground
-  9: [0, 175, 0], # vegetation
-  10: [0, 60, 135], # terrain
-  11: [150, 240, 255], # pole
-  12: [250, 250, 250] # traffic-sign
-}
+    for class_id, color in class_colors.items():
+        visualization[mask == int(class_id)] = color
 
-class_names = {
-  0 : "unlabeled",
-  1 : "car",
-  2 : "bicycle",
-  3 : "motorcycle",
-  4 : "truck",
-  5 : "other-vehicle",
-  6: "person",
-  7: "bicyclist",
-  8: "motorcyclist",
-  9: "road",
-  10: "parking",
-  11: "sidewalk",
-  12: "other-ground",
-  13: "building",
-  14: "fence",
-  15: "vegetation",
-  16: "trunk",
-  17: "terrain",
-  18: "pole",
-  19: "traffic-sign",
-}
+    return visualization
 
-# color_map = {
-#   0 : [0, 0, 0],
-#   1 : [245, 150, 100],
-#   2 : [245, 230, 100],
-#   3 : [150, 60, 30],
-#   4 : [180, 30, 80],
-#   5 : [255, 0, 0],
-#   6: [30, 30, 255],
-#   7: [200, 40, 255],
-#   8: [90, 30, 150],
-#   9: [125,125,125],
-#   10: [255, 150, 255],
-#   11: [75, 0, 75],
-#   12: [75, 0, 175],
-#   13: [0, 200, 255],
-#   14: [50, 120, 255],
-#   15: [0, 175, 0],
-#   16: [0, 60, 135],
-#   17: [80, 240, 150],
-#   18: [150, 240, 255],
-#   19: [250, 250, 250],
-#   20: [0, 250, 0]
-# }
+class MyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        else:
+            return super(MyEncoder, self).default(obj)
 
-# Create the custom color map
-custom_colormap = np.zeros((256, 1, 3), dtype=np.uint8)
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
-for i in range(256):
-    if i in color_map:
-        custom_colormap[i, 0, :] = color_map[i]
-    else:
-        # If the index is not defined in the color map, set it to black
-        custom_colormap[i, 0, :] = [0, 0, 0]
-custom_colormap = custom_colormap[...,::-1]
+class Tester:
+    def __init__(self, model, save_path, config, load=False, visualize=True):
+        self.visualize = visualize
+        self.model = model
+        if load:
+            self.model.load_state_dict(torch.load(save_path))
+
+        self.config = config
+        self.normals = self.config["USE_NORMALS"]
+        self.num_classes = self.config["NUM_CLASSES"]
+        self.class_names = self.config["CLASS_NAMES"]
+        self.class_colors = self.config["CLASS_COLORS"]
+
+        self.save_path = os.path.dirname(save_path)
+        time.sleep(3)
+
+        # Timer
+        self.start = torch.cuda.Event(enable_timing=True)
+        self.end = torch.cuda.Event(enable_timing=True)
+        
+        # Evaluator
+        self.evaluator = SemanticSegmentationEvaluator(num_classes=self.num_classes)
+
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model.to(self.device)
+
+
+    
+    def __call__(self, dataloader_test):
+        self.model.eval()
+        self.evaluator.reset()
+        for batch_idx, (range_img, reflectivity, xyz, normals, semantic)  in enumerate(dataloader_test):
+            range_img, reflectivity, xyz, normals, semantic = range_img.to(self.device), reflectivity.to(self.device), xyz.to(self.device), normals.to(self.device), semantic.to(self.device)
+            start_time = time.time()
+
+            # run forward path
+            start_time = time.time()
+            self.start.record()
+            if self.normals:
+                outputs_semantic = self.model(torch.cat([range_img, reflectivity],axis=1), torch.cat([xyz, normals],axis=1))
+            else:
+                outputs_semantic = self.model(torch.cat([range_img, reflectivity],axis=1), xyz)
+            self.end.record()
+            curr_time = (time.time()-start_time)*1000
+    
+            # Waits for everything to finish running
+            torch.cuda.synchronize()
+            
+            semseg_img = torch.argmax(outputs_semantic,dim=1)
+
+            if self.visualize:
+                # visualize first sample in batch
+                semantics_pred = (semseg_img).permute(0, 1, 2)[0,...].cpu().detach().numpy()
+                semantics_gt = (semantic[:,0,:,:]).permute(0, 1, 2)[0,...].cpu().detach().numpy()
+                xyz_img = (xyz).permute(0, 2, 3, 1)[0,...].cpu().detach().numpy()
+                normal_img = (normals.permute(0, 2, 3, 1)[0,...].cpu().detach().numpy()+1)/2
+                reflectivity_img = (reflectivity).permute(0, 2, 3, 1)[0,...].cpu().detach().numpy()
+                reflectivity_img = np.uint8(255*np.concatenate(3*[reflectivity_img],axis=-1))
+                prev_sem_pred = visualize_semantic_segmentation_cv2(semantics_pred, class_colors=self.class_colors)
+                prev_sem_gt = visualize_semantic_segmentation_cv2(semantics_gt, class_colors=self.class_colors)
+                cv2.imshow("inf", np.vstack((reflectivity_img,np.uint8(255*normal_img),prev_sem_pred,prev_sem_gt)))
+                cv2.waitKey(1)
+
+            self.evaluator.update(semseg_img, semantic)
+        mIoU, result_dict = self.evaluator.compute_final_metrics(class_names=self.class_names)
+        with open(os.path.join(self.save_path, "results.json"), 'w') as fp:
+            json.dump(result_dict, fp, cls=MyEncoder)
+ 
+
+    
+    
+
