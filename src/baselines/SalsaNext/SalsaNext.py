367919947ef6fee18cf01096e353cb9daffd8e53
turacan <kerim.turacan@outlook.com>
2025-08-20 14:50:11 +0000

    added salsanext method

---
+++ src/baselines/SalsaNext/SalsaNext.py
@@ -0,0 +1,269 @@
+"""
+Source code from original SalsaNext github repo
+https://github.com/TiagoCortinhal/SalsaNext/blob/7548c124b48f0259cdc40e98dfc3aeeadca6070c/train/tasks/semantic/modules/SalsaNext.py
+"""
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class ResContextBlock(nn.Module):
+    def __init__(self, in_filters, out_filters):
+        super(ResContextBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_filters, out_filters, kernel_size=(1, 1), stride=1)
+        self.act1 = nn.LeakyReLU()
+
+        self.conv2 = nn.Conv2d(out_filters, out_filters, (3,3), padding=1)
+        self.act2 = nn.LeakyReLU()
+        self.bn1 = nn.BatchNorm2d(out_filters)
+
+        self.conv3 = nn.Conv2d(out_filters, out_filters, (3,3),dilation=2, padding=2)
+        self.act3 = nn.LeakyReLU()
+        self.bn2 = nn.BatchNorm2d(out_filters)
+
+
+    def forward(self, x):
+
+        shortcut = self.conv1(x)
+        shortcut = self.act1(shortcut)
+
+        resA = self.conv2(shortcut)
+        resA = self.act2(resA)
+        resA1 = self.bn1(resA)
+
+        resA = self.conv3(resA1)
+        resA = self.act3(resA)
+        resA2 = self.bn2(resA)
+
+        output = shortcut + resA2
+        return output
+
+
+class ResBlock(nn.Module):
+    def __init__(self, in_filters, out_filters, dropout_rate, kernel_size=(3, 3), stride=1,
+                 pooling=True, drop_out=True):
+        super(ResBlock, self).__init__()
+        self.pooling = pooling
+        self.drop_out = drop_out
+        self.conv1 = nn.Conv2d(in_filters, out_filters, kernel_size=(1, 1), stride=stride)
+        self.act1 = nn.LeakyReLU()
+
+        self.conv2 = nn.Conv2d(in_filters, out_filters, kernel_size=(3,3), padding=1)
+        self.act2 = nn.LeakyReLU()
+        self.bn1 = nn.BatchNorm2d(out_filters)
+
+        self.conv3 = nn.Conv2d(out_filters, out_filters, kernel_size=(3,3),dilation=2, padding=2)
+        self.act3 = nn.LeakyReLU()
+        self.bn2 = nn.BatchNorm2d(out_filters)
+
+        self.conv4 = nn.Conv2d(out_filters, out_filters, kernel_size=(2, 2), dilation=2, padding=1)
+        self.act4 = nn.LeakyReLU()
+        self.bn3 = nn.BatchNorm2d(out_filters)
+
+        self.conv5 = nn.Conv2d(out_filters*3, out_filters, kernel_size=(1, 1))
+        self.act5 = nn.LeakyReLU()
+        self.bn4 = nn.BatchNorm2d(out_filters)
+
+        if pooling:
+            self.dropout = nn.Dropout2d(p=dropout_rate)
+            self.pool = nn.AvgPool2d(kernel_size=kernel_size, stride=2, padding=1)
+        else:
+            self.dropout = nn.Dropout2d(p=dropout_rate)
+
+    def forward(self, x):
+        shortcut = self.conv1(x)
+        shortcut = self.act1(shortcut)
+
+        resA = self.conv2(x)
+        resA = self.act2(resA)
+        resA1 = self.bn1(resA)
+
+        resA = self.conv3(resA1)
+        resA = self.act3(resA)
+        resA2 = self.bn2(resA)
+
+        resA = self.conv4(resA2)
+        resA = self.act4(resA)
+        resA3 = self.bn3(resA)
+
+        concat = torch.cat((resA1,resA2,resA3),dim=1)
+        resA = self.conv5(concat)
+        resA = self.act5(resA)
+        resA = self.bn4(resA)
+        resA = shortcut + resA
+
+
+        if self.pooling:
+            if self.drop_out:
+                resB = self.dropout(resA)
+            else:
+                resB = resA
+            resB = self.pool(resB)
+
+            return resB, resA
+        else:
+            if self.drop_out:
+                resB = self.dropout(resA)
+            else:
+                resB = resA
+            return resB
+
+
+class UpBlock(nn.Module):
+    def __init__(self, in_filters, out_filters, dropout_rate, drop_out=True):
+        super(UpBlock, self).__init__()
+        self.drop_out = drop_out
+        self.in_filters = in_filters
+        self.out_filters = out_filters
+
+        self.dropout1 = nn.Dropout2d(p=dropout_rate)
+
+        self.dropout2 = nn.Dropout2d(p=dropout_rate)
+
+        self.conv1 = nn.Conv2d(in_filters//4 + 2*out_filters, out_filters, (3,3), padding=1)
+        self.act1 = nn.LeakyReLU()
+        self.bn1 = nn.BatchNorm2d(out_filters)
+
+        self.conv2 = nn.Conv2d(out_filters, out_filters, (3,3),dilation=2, padding=2)
+        self.act2 = nn.LeakyReLU()
+        self.bn2 = nn.BatchNorm2d(out_filters)
+
+        self.conv3 = nn.Conv2d(out_filters, out_filters, (2,2), dilation=2,padding=1)
+        self.act3 = nn.LeakyReLU()
+        self.bn3 = nn.BatchNorm2d(out_filters)
+
+
+        self.conv4 = nn.Conv2d(out_filters*3,out_filters,kernel_size=(1,1))
+        self.act4 = nn.LeakyReLU()
+        self.bn4 = nn.BatchNorm2d(out_filters)
+
+        self.dropout3 = nn.Dropout2d(p=dropout_rate)
+
+    def forward(self, x, skip):
+        upA = nn.PixelShuffle(2)(x)
+        if self.drop_out:
+            upA = self.dropout1(upA)
+
+        upB = torch.cat((upA,skip),dim=1)
+        if self.drop_out:
+            upB = self.dropout2(upB)
+
+        upE = self.conv1(upB)
+        upE = self.act1(upE)
+        upE1 = self.bn1(upE)
+
+        upE = self.conv2(upE1)
+        upE = self.act2(upE)
+        upE2 = self.bn2(upE)
+
+        upE = self.conv3(upE2)
+        upE = self.act3(upE)
+        upE3 = self.bn3(upE)
+
+        concat = torch.cat((upE1,upE2,upE3),dim=1)
+        upE = self.conv4(concat)
+        upE = self.act4(upE)
+        upE = self.bn4(upE)
+        if self.drop_out:
+            upE = self.dropout3(upE)
+
+        return upE
+
+
+class SalsaNext(nn.Module):
+    def __init__(self, nclasses, nchannels=5):
+        super(SalsaNext, self).__init__()
+        self.nclasses = nclasses
+
+        self.downCntx = ResContextBlock(nchannels, 32)
+        self.downCntx2 = ResContextBlock(32, 32)
+        self.downCntx3 = ResContextBlock(32, 32)
+
+        self.resBlock1 = ResBlock(32, 2 * 32, 0.2, pooling=True, drop_out=False)
+        self.resBlock2 = ResBlock(2 * 32, 2 * 2 * 32, 0.2, pooling=True)
+        self.resBlock3 = ResBlock(2 * 2 * 32, 2 * 4 * 32, 0.2, pooling=True)
+        self.resBlock4 = ResBlock(2 * 4 * 32, 2 * 4 * 32, 0.2, pooling=True)
+        self.resBlock5 = ResBlock(2 * 4 * 32, 2 * 4 * 32, 0.2, pooling=False)
+
+        self.upBlock1 = UpBlock(2 * 4 * 32, 4 * 32, 0.2)
+        self.upBlock2 = UpBlock(4 * 32, 4 * 32, 0.2)
+        self.upBlock3 = UpBlock(4 * 32, 2 * 32, 0.2)
+        self.upBlock4 = UpBlock(2 * 32, 32, 0.2, drop_out=False)
+
+        self.logits = nn.Conv2d(32, nclasses, kernel_size=(1, 1))
+
+    def forward_logits(self, x):  # <-- NEW: returns raw logits
+        downCntx = self.downCntx(x)
+        downCntx = self.downCntx2(downCntx)
+        downCntx = self.downCntx3(downCntx)
+
+        down0c, down0b = self.resBlock1(downCntx)
+        down1c, down1b = self.resBlock2(down0c)
+        down2c, down2b = self.resBlock3(down1c)
+        down3c, down3b = self.resBlock4(down2c)
+        down5c = self.resBlock5(down3c)
+
+        up4e = self.upBlock1(down5c,down3b)
+        up3e = self.upBlock2(up4e, down2b)
+        up2e = self.upBlock3(up3e, down1b)
+        up1e = self.upBlock4(up2e, down0b)
+        logits = self.logits(up1e)
+
+        # NOTE: removed the following 2 lines from original SalsaNext implementation to yield both logits and softmax-logits
+        # logits = logits
+        # logits = F.softmax(logits, dim=1)
+        return logits
+    
+    def forward(self, x):  # keep original SalsaNext behavior for training
+        logits = self.forward_logits(x)
+        return F.softmax(logits, dim=1)
+
+
+class TemperatureScaling(nn.Module):
+    def __init__(self, model, init_T=1.0):
+        super().__init__()
+        self.model = model           # base model (already trained)
+        self.log_T = nn.Parameter(torch.log(torch.tensor([init_T], dtype=torch.float32)))
+
+    def temperature(self):
+        # strictly positive T
+        return torch.exp(self.log_T)
+
+    @torch.no_grad()
+    def logits(self, *model_args, **model_kwargs):
+        # Detach base-model logits so we optimize ONLY T
+        logits = self.model.forward_logits(*model_args, **model_kwargs)
+        return logits.detach()
+
+    def forward(self, *model_args, return_probs=True, **model_kwargs):
+        logits = self.logits(*model_args, **model_kwargs)
+        T = self.temperature().clamp_min(1e-3)
+        scaled = logits / T
+        if return_probs:
+            return F.softmax(scaled, dim=1)
+        return scaled
+
+
+if __name__ == "__main__":
+    import time
+    import numpy as np
+    model = SalsaNext(20).cuda()
+    pytorch_total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    print("Number of parameters: ", pytorch_total_params / 1000000, "M")
+    # Timer
+    start = torch.cuda.Event(enable_timing=True)
+    end = torch.cuda.Event(enable_timing=True)
+    inference_times = []
+    model.eval()
+    with torch.no_grad():
+        for i in range(100):
+            inputs = torch.randn(1, 5, 128, 2048).cuda()
+            start.record()
+            outputs = model(inputs)
+            end.record()
+            torch.cuda.synchronize()  # wait for cuda to finish (cuda is asynchronous!)
+            print("inference took {} ms".format(start.elapsed_time(end)))
+            
+            inference_times.append(start.elapsed_time(end))
+            #time.sleep(0.15)
+    print("inference mean {} ms".format(np.mean(inference_times)))
